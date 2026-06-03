53d11328dc9d24cf0af861e296fe91484a541d84
crucioltheresa <mtcruciol@icloud.com>
2026-06-03 18:09:13 +0100

    Add Stripe integration: implement Stripe elements for payment processing, update checkout template with necessary scripts and context variables.

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = "checkout/checkout.html"
     context = {
         "order_form": order_form,
+        "stripe_public_key": "pk_test_51TdyJ3KOk1eNwg7UUq1G0N5BTr0OaQcl4bHh8PVPg8PreurMWDVLyay7OD2aQknOok91xAs07MndJ5KfcXX73jtb00DMlF94zJ",
+        "client_secret": "test_client_secret",
     }
 
     return render(request, template, context)
