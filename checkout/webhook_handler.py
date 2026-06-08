0d5099d63bb441ecd5f69d7ca0aafe921f78e7e5
crucioltheresa <mtcruciol@icloud.com>
2026-06-08 18:40:56 +0100

    Add Stripe webhook handler: implement class to manage webhook events and return responses

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+
+
+class StripeWH_Handler:
+    """Handler Stripe webhooks"""
+
+    def __init__(self, request):
+        self.request = request
+
+    def handler_event(self, event):
+        """Handle a generic/unknown/unexpected webhook event"""
+        return HttpResponse(content=f'Webhook received: {event["type"]}', status=200)
