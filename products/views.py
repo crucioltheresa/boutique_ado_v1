569d72b990d121a14038fdd3cd0882761e76f0c4
crucioltheresa <mtcruciol@icloud.com>
2026-06-17 20:42:35 +0100

    Add image upload functionality to product forms and templates

--- products/views.py
+++ products/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from .models import Product, Category
 from django.db.models.functions import Lower
+
+from .models import Product, Category
 from .forms import ProductForm
 
-
 # Create your views here.
+
+
 def all_products(request):
     """A view to show all products, including sorting and search queries"""
 
@@ -26,7 +28,6 @@ def all_products(request):
                 products = products.annotate(lower_name=Lower("name"))
             if sortkey == "category":
                 sortkey = "category__name"
-
             if "direction" in request.GET:
                 direction = request.GET["direction"]
                 if direction == "desc":
@@ -61,7 +62,9 @@ def all_products(request):
 
 def product_detail(request, product_id):
     """A view to show individual product details"""
+
     product = get_object_or_404(Product, pk=product_id)
+
     context = {
         "product": product,
     }
@@ -80,9 +83,7 @@ def add_product(request):
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             product = form.save()
-            messages.success(
-                request, "Successfully added product!", extra_tags="product_operation"
-            )
+            messages.success(request, "Successfully added product!")
             return redirect(reverse("product_detail", args=[product.id]))
         else:
             messages.error(
@@ -103,21 +104,19 @@ def add_product(request):
 def edit_product(request, product_id):
     """Edit a product in the store"""
     if not request.user.is_superuser:
-    messages.error(request, "Sorry, only store owners can do that.")
-    return redirect(reverse("home"))
+        messages.error(request, "Sorry, only store owners can do that.")
+        return redirect(reverse("home"))
 
     product = get_object_or_404(Product, pk=product_id)
     if request.method == "POST":
         form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
             form.save()
-            messages.success(
-                request, "Successfully updated product!", extra_tags="product_operation"
-            )
+            messages.success(request, "Successfully updated product!")
             return redirect(reverse("product_detail", args=[product.id]))
         else:
             messages.error(
-                request, "Failed to update product. Please ensure the form is valid"
+                request, "Failed to update product. Please ensure the form is valid."
             )
     else:
         form = ProductForm(instance=product)
@@ -141,5 +140,5 @@ def delete_product(request, product_id):
 
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
-    messages.success(request, "Product deleted!", extra_tags="product_operation")
+    messages.success(request, "Product deleted!")
     return redirect(reverse("products"))
