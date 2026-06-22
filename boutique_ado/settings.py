3d858074d2b6effc03b7d17dd54131ac61bcad51
crucioltheresa <mtcruciol@icloud.com>
2026-06-22 21:11:07 +0100

    Add Procfile, runtime.txt, and update requirements for deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 import os
+import dj_database_url
+
 from pathlib import Path
 
 if os.path.isfile("env.py"):
@@ -32,6 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
+    "boutique-ado-mt-bb71af4e5c6e.herokuapp.com",
 ]
 
 
@@ -125,12 +128,15 @@ WSGI_APPLICATION = "boutique_ado.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if "DATABASE_URL" in os.environ:
+    DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
 
 
 # Password validation
